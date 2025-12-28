41ee027603ae186f889b7712581294721d703d64
Dev Dork <robertcjohnson1984@gmail.com>
2025-12-28 16:09:47 -0700

    chore(update) configs

--- babel.config.js
+++ babel.config.js
@@ -6,20 +6,20 @@ module.exports = function (api) {
       ...(process.env.TAMAGUI_DISABLE === "1"
         ? []
         : [
-            [
-              "@tamagui/babel-plugin",
-              {
-                components: ["tamagui"],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
                 // Use a CJS config so Tamagui can load it during static extraction on Vercel.
                 config: "./tamagui.config.cjs",
-                logTimings: true,
+          logTimings: true,
                 // Static extraction can be very memory hungry (especially in CI/Vercel).
                 // Allow explicitly disabling it via env var.
                 disableExtraction:
                   process.env.NODE_ENV === "development" ||
                   process.env.TAMAGUI_DISABLE_EXTRACTION === "1",
-              },
-            ],
+        },
+      ],
           ]),
 
       // NOTE: this is only necessary if you are using reanimated for animations
--- metro.config.js
+++ metro.config.js
@@ -12,13 +12,13 @@ const config = getDefaultConfig(__dirname, {
 if (process.env.TAMAGUI_DISABLE === '1') {
   module.exports = config
 } else {
-  // add nice web support with optimizing compiler + CSS extraction
-  const { withTamagui } = require('@tamagui/metro-plugin')
-  module.exports = withTamagui(config, {
-    components: ['tamagui'],
+// add nice web support with optimizing compiler + CSS extraction
+const { withTamagui } = require('@tamagui/metro-plugin')
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
     // Use a CJS config so Tamagui can load it during static extraction on Vercel.
     config: './tamagui.config.cjs',
-    outputCSS: './tamagui-web.css',
-    cssInterop: true,
-  })
+  outputCSS: './tamagui-web.css',
+  cssInterop: true,
+})
 }
